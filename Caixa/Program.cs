467328fa0b48d5b546d1d38f09a6dca77a8656f2
Lucas Scquiavon <118318155+LucasFnavarro@users.noreply.github.com>
2024-12-02 15:08:50 -0300

    primeiro

---
+++ Caixa/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+/*
+    Usuário possui uma conta com saldo,
+    Pode sacar e depositar o valor.
+*/
+
+namespace Caixa
+{
+    class Program
+    {
+        static decimal saldoDisponivel = 0;
+
+        static void Main(string[] args)
+        {
+            MenuOperacoes();
+        }
+
+        public static void MenuOperacoes()
+        {
+            Console.Clear();
+            Console.WriteLine("Qual operação deseja realizar?");
+            Console.WriteLine($"1 - Ver saldo\n2- Fazer depósito\n3 - Realizar saque\n0 - Para sair");
+            int opcaoMenu = int.Parse(Console.ReadLine());
+
+            switch (opcaoMenu)
+            {
+                case 0: Environment.Exit(0); break;
+                case 1: VerSaldoEmConta(); break;
+                case 2: RealizarDeposito(); break;
+                case 3: RealizarSaque(); break;
+                default:
+                    Console.WriteLine("Opção inválida, tente novamente!");
+                    MenuOperacoes();
+                    break;
+            }
+        }
+
+        public static void VerSaldoEmConta()
+        {
+            Console.WriteLine($"Você tem disponivel para saque: {saldoDisponivel.ToString("C")}");
+            Console.WriteLine("Aperte qualquer tecla para continuar...");
+            Console.ReadKey();
+            MenuOperacoes();
+        }
+
+        public static void RealizarDeposito()
+        {
+            Console.WriteLine("Qual o valor deseja depositar: R$");
+            decimal valorDeposito = decimal.Parse(Console.ReadLine());
+
+            if (valorDeposito > 0)
+            {
+                saldoDisponivel += valorDeposito;
+                Console.WriteLine($"VALOR DEPOSITADO: {valorDeposito:C}");
+                Console.WriteLine($"\nSaldo total: {saldoDisponivel:C}");
+            }
+            else
+            {
+                Console.WriteLine("Por favor, digite um valor válido para depositar...");
+            }
+
+            Console.WriteLine("Aperte qualquer tecla para continuar...");
+            Console.ReadKey();
+            MenuOperacoes();
+        }
+
+        public static void RealizarSaque()
+        {
+            Console.Write("Insira um valor para sacar: R$");
+            decimal valorDeSaque = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nSAQUE SOLICITADO: {valorDeSaque:C}");
+
+            if (valorDeSaque > saldoDisponivel)
+            {
+                Console.WriteLine($"Por favor, insira um valor válido, seu saldo é {saldoDisponivel:C}");
+                Console.ReadKey();
+                MenuOperacoes();
+            }
+
+            saldoDisponivel -= valorDeSaque;
+            Console.WriteLine("Saque realizado com sucesso.");
+            Console.WriteLine($"SEU SALDO ATUAL: {saldoDisponivel:C}");
+            Console.WriteLine("Aperte qualquer tecla para continuar...");
+            Console.ReadKey();
+            MenuOperacoes();
+
+        }
+    }
+}
---
+++ InserirNumeroNoFimDoArray/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+/* Crie uma matriz linear com 6 números e adicione o número 75 no final dela */
+
+namespace InserirNumeroNoFimDoArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = [10, 20, 30, 40, 50];
+            array = AddNumeroNoArray(array, 75);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"{array[i]}" + ",");
+            }
+
+
+
+            string[] alunos = { "Lucas", "Maria", "José", "Fernando" };
+            alunos = AddNome(alunos, "Vitória");
+
+            for (int i = 0; i < alunos.Length; i++)
+            {
+                Console.WriteLine(alunos[i] + ",");
+            }
+
+        }
+
+        public static string[] AddNome(string[] alunos, string aluno)
+        {
+            string[] nomesTemp = new string[alunos.Length + 1];
+
+            for (int i = 0; i < alunos.Length; i++)
+            {
+                nomesTemp[i] = alunos[i];
+            }
+
+            nomesTemp[alunos.Length] = aluno;
+            return nomesTemp;
+
+
+        }
+
+        public static int[] AddNumeroNoArray(int[] array, int value)
+        {
+            int[] tempArray = new int[array.Length + 1];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                tempArray[i] = array[i];
+            }
+
+            tempArray[array.Length] = value;
+            return tempArray;
+        }
+    }
+}
